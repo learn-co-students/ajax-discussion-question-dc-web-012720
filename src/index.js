968721b23207bcc15668e3ec0e531f2ce6d7d4f2
bumbot <pkim1295@gmail.com>
2020-03-13 09:15:22 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,26 @@ console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate 
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
+
+  const button = document.querySelector("button");
+
+  button.addEventListener("click", fetchData);
+
 });
+
+function fetchData() {
+  fetch('https://randomuser.me/api/')
+    .then(resp => resp.json())
+    .then(json => addPerson(json))
+}
+
+function addPerson(json) {
+  debugger
+
+  const firstName = json.results[0].name.first;
+  const lastName = json.results[0].name.last;
+  const profilePic = json.results[0].picture.medium;
+
+  const street = json.results[0].location.street.number + json.results[0].location.street.name;
+   
+}
